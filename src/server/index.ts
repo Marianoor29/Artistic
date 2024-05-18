8a1c86bc04788c650b55989891df93f2ec90803e
Maria Noor <76403209+Marianoor29@users.noreply.github.com>
2024-05-18 19:38:31 +0100

    remove firebase

---
+++ src/server/index.ts
@@ -0,0 +1,16 @@
+const express = require('express')
+const port = 3000
+
+const app = express()
+const bodyParser = require('body-parser')
+
+
+app.use(bodyParser.json())
+
+app.get('/',(req, res) => {
+res.send('hellow artistic')
+})
+
+app.listen(port, () =>{
+    console.log(`server is running on port ${port}`)
+})
